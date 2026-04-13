b1a0bce9d1d9418a3caa0322663789eacf3b10c5
Seongmin Jin <dprtjqj@gmail.com>
2026-04-13 10:12:52 +0900

    design fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css' // CSS는 가장 아래에서 한 번만!
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
